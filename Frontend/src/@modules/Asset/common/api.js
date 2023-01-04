1e1ce6e367f87bc1969f30ee68823c9a5d23715e
callmebaoxxxx <vuna@friday.vn>
2023-01-04 17:33:51 +0700

    fix router

--- Frontend/src/@modules/Asset/common/api.js
+++ Frontend/src/@modules/Asset/common/api.js
@@ -79,45 +79,47 @@ export const assetApi = {
   // ** asset group api
   async createAssetGroup(data) {
     return await axiosApi.post(
-      "asset-groups/create",
+      "asset-asset-group/create",
       serialize(_.cloneDeep(data))
     )
   },
   async getDataAssetGroup(params) {
     const strParam = object2QueryString(params)
     return await axiosApi.get(
-      `asset-groups/get-data-asset-group?get${strParam}`
+      `asset-asset-group/get-data-asset-group?get${strParam}`
     )
   },
   async updateAssetGroup(id, data) {
     return await axiosApi.post(
-      `asset-groups/update/${id}`,
+      `asset-asset-group/update/${id}`,
       serialize(_.cloneDeep(data))
     )
   },
   async deleteAssetGroup(id) {
-    return await axiosApi.post(`asset-groups/delete/${id}`)
+    return await axiosApi.post(`asset-asset-group/delete/${id}`)
   },
 
   // ** asset type api
   async createAssetType(data) {
     return await axiosApi.post(
-      "asset-types/create",
+      "asset-asset-type/create",
       serialize(_.cloneDeep(data))
     )
   },
   async getDataAssetType(params) {
     const strParam = object2QueryString(params)
-    return await axiosApi.get(`asset-types/get-data-asset-type?get${strParam}`)
+    return await axiosApi.get(
+      `asset-asset-type/get-data-asset-type?get${strParam}`
+    )
   },
   async updateAssetType(id, data) {
     return await axiosApi.post(
-      `asset-types/update/${id}`,
+      `asset-asset-type/update/${id}`,
       serialize(_.cloneDeep(data))
     )
   },
   async deleteAssetType(id) {
-    return await axiosApi.post(`asset-types/delete/${id}`)
+    return await axiosApi.post(`asset-asset-type/delete/${id}`)
   }
 }
 
