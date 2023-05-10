c890d1dc03177e43324559a6e506405e3ca58c41
Thang Nguyen <thangnt@friday.vn>
2023-05-10 17:27:42 +0700

    stop

--- Frontend/src/components/hrm/CreatePost/CreatePostDetails/EditorComponent.js
+++ Frontend/src/components/hrm/CreatePost/CreatePostDetails/EditorComponent.js
@@ -2,27 +2,23 @@ import { useFormatMessage, useMergedState } from "@apps/utility/common"
 import createLinkPlugin from "@draft-js-plugins/anchor"
 import {
   BoldButton,
-  CodeBlockButton,
   ItalicButton,
-  OrderedListButton,
-  UnderlineButton,
-  UnorderedListButton
+  UnderlineButton
 } from "@draft-js-plugins/buttons"
 import Editor from "@draft-js-plugins/editor"
 import createInlineToolbarPlugin from "@draft-js-plugins/inline-toolbar"
+import createLinkifyPlugin from "@draft-js-plugins/linkify"
 import createMentionPlugin, {
   defaultSuggestionsFilter
 } from "@draft-js-plugins/mention"
-import createToolbarPlugin from "@draft-js-plugins/static-toolbar"
-import { useCallback, useEffect, useMemo, Fragment } from "react"
-import createLinkifyPlugin from "@draft-js-plugins/linkify"
+import { Fragment, useCallback, useEffect, useMemo } from "react"
 
-import "@styles/react/libs/editor/editor.scss"
 import "@draft-js-plugins/inline-toolbar/lib/plugin.css"
+import "@draft-js-plugins/linkify/lib/plugin.css"
 import "@draft-js-plugins/mention/lib/plugin.css"
 import "@draft-js-plugins/static-toolbar/lib/plugin.css"
-import "@draft-js-plugins/linkify/lib/plugin.css"
 import { arrImage } from "@modules/Feed/common/common"
+import "@styles/react/libs/editor/editor.scss"
 
 const EditorComponent = (props) => {
   const {
@@ -61,13 +57,10 @@ const EditorComponent = (props) => {
   }, [backgroundImage, showChooseBackgroundImage])
 
   // ** mention
-  const { plugins, MentionSuggestions, Toolbar, InlineToolbar, linkPlugin } =
+  const { plugins, MentionSuggestions, InlineToolbar, linkPlugin } =
     useMemo(() => {
       const mentionPlugin = createMentionPlugin()
       const { MentionSuggestions } = mentionPlugin
-
-      const staticToolbarPlugin = createToolbarPlugin()
-      const { Toolbar } = staticToolbarPlugin
 
       const inlineToolbarPlugin = createInlineToolbarPlugin()
       const { InlineToolbar } = inlineToolbarPlugin
@@ -77,12 +70,11 @@ const EditorComponent = (props) => {
 
       const plugins = [
         mentionPlugin,
-        staticToolbarPlugin,
         inlineToolbarPlugin,
         linkPlugin,
         linkifyPlugin
       ]
-      return { plugins, MentionSuggestions, Toolbar, InlineToolbar, linkPlugin }
+      return { plugins, MentionSuggestions, InlineToolbar, linkPlugin }
     }, [])
   const onOpenChange = useCallback((_open) => {
     setTimeout(() => {
@@ -110,7 +102,7 @@ const EditorComponent = (props) => {
   return (
     <div
       className={`div-editor ${
-        backgroundImage !== null && "div-editor-background"
+        backgroundImage !== null ? "div-editor-background" : ""
       }`}
       style={renderStyleBackgroundImage()}>
       <Editor
@@ -124,22 +116,6 @@ const EditorComponent = (props) => {
       />
 
       <div id="div-tool-bar">
-        <Toolbar>
-          {
-            // may be use React.Fragment instead of div to improve performance after React 16
-            (externalProps) => (
-              <Fragment>
-                <BoldButton {...externalProps} />
-                <ItalicButton {...externalProps} />
-                <UnderlineButton {...externalProps} />
-                <UnorderedListButton {...externalProps} />
-                <OrderedListButton {...externalProps} />
-                <CodeBlockButton {...externalProps} />
-              </Fragment>
-            )
-          }
-        </Toolbar>
-
         <InlineToolbar>
           {
             // may be use React.Fragment instead of div to improve performance after React 16
--- Frontend/src/components/hrm/CreatePost/CreatePostDetails/PollVote.js
+++ Frontend/src/components/hrm/CreatePost/CreatePostDetails/PollVote.js
@@ -29,7 +29,8 @@ const PollVote = (props) => {
       limit_time: false
     },
     time_end: null,
-    disable_btn_continue: true
+    disable_btn_continue: true,
+    modalPollVoteSetting: false
   })
 
   const refInputAddOptions = useRef(null)
@@ -62,6 +63,9 @@ const PollVote = (props) => {
     options.splice(index, 1)
     setState({ options: options })
   }
+
+  const toggleModalPollVoteSetting = () =>
+    setState({ modalPollVoteSetting: !state.modalPollVoteSetting })
 
   // useEffect
   useEffect(() => {
@@ -156,15 +160,35 @@ const PollVote = (props) => {
             </span>
           </div>
           <div className="body-content">
-            <ErpInput
-              label={useFormatMessage("modules.feed.create_post.text.question")}
-              required
-              placeholder={useFormatMessage(
-                "modules.feed.create_post.text.enter_question"
-              )}
-              value={state.question}
-              onChange={(e) => setState({ question: e.target.value })}
-            />
+            <div className="div-question">
+              <ErpInput
+                label={useFormatMessage(
+                  "modules.feed.create_post.text.question"
+                )}
+                required
+                placeholder={useFormatMessage(
+                  "modules.feed.create_post.text.enter_question"
+                )}
+                value={state.question}
+                onChange={(e) => setState({ question: e.target.value })}
+              />
+
+              <div
+                className="div-btn-setting"
+                onClick={() => toggleModalPollVoteSetting()}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="21"
+                  height="19"
+                  viewBox="0 0 21 19"
+                  fill="none">
+                  <path
+                    d="M18.1 6.6589C16.29 6.6589 15.55 5.3789 16.45 3.8089C16.97 2.8989 16.66 1.7389 15.75 1.2189L14.02 0.228901C13.23 -0.241099 12.21 0.0389014 11.74 0.828901L11.63 1.0189C10.73 2.5889 9.25 2.5889 8.34 1.0189L8.23 0.828901C7.78 0.0389014 6.76 -0.241099 5.97 0.228901L4.24 1.2189C3.33 1.7389 3.02 2.9089 3.54 3.8189C4.45 5.3789 3.71 6.6589 1.9 6.6589C0.86 6.6589 0 7.5089 0 8.5589V10.3189C0 11.3589 0.85 12.2189 1.9 12.2189C3.71 12.2189 4.45 13.4989 3.54 15.0689C3.02 15.9789 3.33 17.1389 4.24 17.6589L5.97 18.6489C6.76 19.1189 7.78 18.8389 8.25 18.0489L8.36 17.8589C9.26 16.2889 10.74 16.2889 11.65 17.8589L11.76 18.0489C12.23 18.8389 13.25 19.1189 14.04 18.6489L15.77 17.6589C16.68 17.1389 16.99 15.9689 16.47 15.0689C15.56 13.4989 16.3 12.2189 18.11 12.2189C19.15 12.2189 20.01 11.3689 20.01 10.3189V8.5589C20 7.5189 19.15 6.6589 18.1 6.6589ZM10 12.6889C8.21 12.6889 6.75 11.2289 6.75 9.4389C6.75 7.6489 8.21 6.1889 10 6.1889C11.79 6.1889 13.25 7.6489 13.25 9.4389C13.25 11.2289 11.79 12.6889 10 12.6889Z"
+                    fill="#32434F"
+                  />
+                </svg>
+              </div>
+            </div>
 
             <label
               title={useFormatMessage(
@@ -195,14 +219,16 @@ const PollVote = (props) => {
                       }
                     />
 
-                    {state.options.length > 2 && (
-                      <Button.Ripple
+                    <div className="div-btn-delete-options">
+                      <button
                         color="danger"
-                        className="btn-delete-options"
+                        className={classNames("btn-delete-options", {
+                          "visibility-hidden": state.options.length <= 2
+                        })}
                         onClick={() => handleDeleteOptions(index)}>
                         <i className="fa-solid fa-xmark"></i>
-                      </Button.Ripple>
-                    )}
+                      </button>
+                    </div>
                   </div>
                 )
               })}
@@ -213,21 +239,82 @@ const PollVote = (props) => {
               className="btn-add-option"
               onClick={() => handleAddOptions()}
               disabled={state.options.length >= 10}>
-              <i className="fa-regular fa-circle-plus fs-4 me-50"></i>
+              <svg
+                className="me-50"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none">
+                <path
+                  d="M8 12H16"
+                  stroke="#292D32"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M12 16V8"
+                  stroke="#292D32"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M9 22H15C20 22 22 20 22 15V9C22 4 20 2 15 2H9C4 2 2 4 2 9V15C2 20 4 22 9 22Z"
+                  stroke="#292D32"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
               {useFormatMessage("modules.feed.create_post.text.add_options")}
             </button>
 
+            <button
+              type="button"
+              className="btn-post"
+              disabled={state.disable_btn_continue}
+              onClick={() => {
+                setPollVoteDetail({
+                  question: state.question,
+                  options: state.options,
+                  setting: state.setting,
+                  time_end: state.time_end
+                })
+                toggleModalPollVote()
+              }}>
+              {useFormatMessage("modules.feed.create_post.text.continue")}
+            </button>
+          </div>
+        </ModalBody>
+      </Modal>
+
+      <Modal
+        isOpen={state.modalPollVoteSetting}
+        toggle={() => toggleModalPollVoteSetting()}
+        className="modal-dialog-centered feed modal-create-post modal-poll-vote modal-poll-vote-setting"
+        modalTransition={{ timeout: 100 }}
+        backdropTransition={{ timeout: 100 }}
+        /* backdrop={"static"} */
+      >
+        <ModalBody>
+          <div className="body-header">
+            <button
+              className="btn-icon"
+              onClick={() => toggleModalPollVoteSetting()}>
+              <i className="fa-solid fa-arrow-left"></i>
+            </button>
+            <span className="text-title">
+              {useFormatMessage("modules.feed.create_post.text.poll_setting")}
+            </span>
+          </div>
+          <div className="body-content">
             <div className="div-setting">
-              <label
-                title={useFormatMessage("app.setting")}
-                className="form-label">
-                {useFormatMessage("app.setting")}
-              </label>
               <ErpCheckbox
                 label={useFormatMessage(
                   "modules.feed.create_post.text.allow_multiple_selection"
                 )}
-                className="mb-50"
                 checked={state.setting.multiple_selection}
                 onChange={(e) =>
                   handleChangeSetting(e.target.checked, "multiple_selection")
@@ -237,7 +324,6 @@ const PollVote = (props) => {
                 label={useFormatMessage(
                   "modules.feed.create_post.text.allow_adding_more_options"
                 )}
-                className="mb-50"
                 checked={state.setting.adding_more_options}
                 onChange={(e) =>
                   handleChangeSetting(e.target.checked, "adding_more_options")
@@ -247,7 +333,6 @@ const PollVote = (props) => {
                 label={useFormatMessage(
                   "modules.feed.create_post.text.allow_incognito"
                 )}
-                className="mb-50"
                 checked={state.setting.incognito}
                 onChange={(e) =>
                   handleChangeSetting(e.target.checked, "incognito")
@@ -268,32 +353,13 @@ const PollVote = (props) => {
             {state.setting.limit_time && (
               <div className="div-setting-time-end">
                 <ErpDatetime
-                  label={useFormatMessage(
-                    "modules.feed.create_post.text.end_at"
-                  )}
-                  labelInline
+                  nolabel
                   placement="topRight"
                   value={state.time_end}
                   onChange={(e) => setState({ time_end: e })}
                 />
               </div>
             )}
-
-            <button
-              type="button"
-              className="btn-post"
-              disabled={state.disable_btn_continue}
-              onClick={() => {
-                setPollVoteDetail({
-                  question: state.question,
-                  options: state.options,
-                  setting: state.setting,
-                  time_end: state.time_end
-                })
-                toggleModalPollVote()
-              }}>
-              {useFormatMessage("modules.feed.create_post.text.continue")}
-            </button>
           </div>
         </ModalBody>
       </Modal>
