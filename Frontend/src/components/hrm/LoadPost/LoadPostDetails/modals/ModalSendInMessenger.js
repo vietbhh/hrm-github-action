f83e0257477db4af4a4d79df0484dd38de3e4806
Thang Nguyen <thangnt@friday.vn>
2023-06-22 13:52:52 +0700

    stop

--- Frontend/src/components/hrm/LoadPost/LoadPostDetails/modals/ModalSendInMessenger.js
+++ Frontend/src/components/hrm/LoadPost/LoadPostDetails/modals/ModalSendInMessenger.js
@@ -60,6 +60,67 @@ const ModalSendInMessenger = (props) => {
             )}
           />
         </div>
+        <div className="div-send-to">
+          <ErpInput
+            label={useFormatMessage(
+              "modules.feed.post.modal_send_in_messenger.send_to"
+            )}
+            placeholder={useFormatMessage(
+              "modules.feed.post.modal_send_in_messenger.enter_email_or_username"
+            )}
+            prepend={
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="20"
+                viewBox="0 0 20 20"
+                fill="none">
+                <path
+                  d="M9.58341 17.5C13.9557 17.5 17.5001 13.9556 17.5001 9.58334C17.5001 5.21108 13.9557 1.66667 9.58341 1.66667C5.21116 1.66667 1.66675 5.21108 1.66675 9.58334C1.66675 13.9556 5.21116 17.5 9.58341 17.5Z"
+                  stroke="#969191"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M18.3334 18.3333L16.6667 16.6667"
+                  stroke="#969191"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            }
+          />
+        </div>
+        <div className="div-recent">
+          <label className="form-label mb-1">
+            {useFormatMessage(
+              "modules.feed.post.modal_send_in_messenger.recent"
+            )}
+          </label>
+
+          <div className="recent-item">
+            <Avatar className="img" src={""} />
+            <span className="recent-item__text">Life. HR</span>
+            <button className="recent-item__button">Send</button>
+          </div>
+          <div className="recent-item">
+            <Avatar className="img" src={""} />
+            <span className="recent-item__text">Life. HR</span>
+            <button className="recent-item__button">Send</button>
+          </div>
+          <div className="recent-item">
+            <Avatar className="img" src={""} />
+            <span className="recent-item__text">Life. HR</span>
+            <button className="recent-item__button">Send</button>
+          </div>
+          <div className="recent-item">
+            <Avatar className="img" src={""} />
+            <span className="recent-item__text">Life. HR</span>
+            <button className="recent-item__button">Send</button>
+          </div>
+        </div>
       </ModalBody>
     </Modal>
   )
