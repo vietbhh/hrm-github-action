ed867567a2bd9ca1dbc282cf3f5cec3c0c4f5758
Thang Nguyen <thangnt@friday.vn>
2023-06-26 12:00:40 +0700

    xong fix chat - dua function sendMessage ra ngoai

--- Frontend/src/@apps/modules/chat/pages/index.js
+++ Frontend/src/@apps/modules/chat/pages/index.js
@@ -1,12 +1,6 @@
 // ** Imports
-import {
-  getPublicDownloadUrl,
-  useFormatMessage,
-  useMergedState
-} from "@apps/utility/common"
+import { useFormatMessage, useMergedState } from "@apps/utility/common"
 import classnames from "classnames"
-import { AES } from "crypto-js"
-import moment from "moment"
 import { Fragment, useContext, useEffect, useRef, useState } from "react"
 import ReactDOM from "react-dom"
 import { IdleTimerProvider } from "react-idle-timer"
@@ -15,7 +9,7 @@ import { useParams } from "react-router-dom"
 import { handleChats, handleTitleChat } from "redux/chat"
 import SocketContext from "utility/context/Socket"
 import { ChatApi } from "../common/api"
-import { imageGroup, replaceHtmlMessage, triGram } from "../common/common"
+import { triGram } from "../common/common"
 
 // ** Chat App Component Imports
 import Chat from "../components/Chat"
@@ -25,18 +19,14 @@ import UserProfileSidebar from "../components/UserProfileSidebar"
 // ** firebase
 import { db } from "firebase"
 import {
-  addDoc,
   arrayRemove,
   collection,
-  deleteDoc,
   doc,
-  getDoc,
   getDocs,
   limit,
   onSnapshot,
   orderBy,
   query,
-  setDoc,
   startAt,
   updateDoc,
   where
@@ -46,7 +36,14 @@ import {
 import "@styles/base/pages/app-chat-list.scss"
 import "@styles/base/pages/app-chat.scss"
 import "../assets/scss/chat.scss"
-import { handleCountFile } from "../common/firebaseCommon"
+import {
+  handleAddNewGroup,
+  handleCountFile,
+  handleSendMessage,
+  handleTimestampFile,
+  handleUpdateGroup,
+  setDataUnseenDetail
+} from "../common/firebaseCommon"
 
 const AppChat = (props) => {
   const [store, setStore] = useMergedState({
@@ -404,448 +401,23 @@ const AppChat = (props) => {
     setContacts(contacts)
   }
 
-  const handleAddNewGroup = async (docData) => {
-    docData = {
-      mute: [],
-      pin: [],
-      avatar: "",
-      background: "",
-      file_count: {},
-      des: "",
-      ...docData
-    }
-    return await addDoc(
-      collection(db, `${firestoreDb}/chat_groups/groups`),
-      docData
-    )
-  }
-
-  const handleUpdateGroup = async (groupId, dataUpdate) => {
-    await updateDoc(
-      doc(db, `${firestoreDb}/chat_groups/groups`, groupId),
-      dataUpdate
-    )
-  }
-
-  const handleDeleteGroup = (groupId) => {
-    deleteDoc(doc(db, `${firestoreDb}/chat_groups/groups`, groupId))
-  }
-
-  const handleLastMessage = (type, msg) => {
-    let last_message = msg
-    if (type === "image" || type === "image_gif") {
-      last_message = useFormatMessage("modules.chat.text.send") + " image"
-    } else if (type === "video") {
-      last_message = useFormatMessage("modules.chat.text.send") + " video"
-    } else if (type === "audio") {
-      last_message = useFormatMessage("modules.chat.text.send") + " audio"
-    } else if (type === "file") {
-      last_message = useFormatMessage("modules.chat.text.send") + " file"
-    } else if (type === "gif") {
-      last_message = useFormatMessage("modules.chat.text.send") + " gif"
-    }
-
-    return last_message
-  }
-
-  const setDataUnseenDetail = (
-    action,
-    user_id,
-    timestamp,
-    unseen_detail,
-    user,
-    member_add = [],
-    member_remove = ""
-  ) => {
-    if (action === "add") {
-      const _unseen_detail = []
-      _.forEach(user, (item) => {
-        if (user_id === item) {
-          _unseen_detail.push({
-            user_id: item,
-            timestamp_from: 0,
-            timestamp_to: 0,
-            unread_count: 0
-          })
-        } else {
-          _unseen_detail.push({
-            user_id: item,
-            timestamp_from: timestamp,
-            timestamp_to: timestamp,
-            unread_count: 1
-          })
-        }
-      })
-
-      return _unseen_detail
-    }
-
-    if (action === "update") {
-      const _unseen_detail = []
-      _.forEach(unseen_detail, (item) => {
-        if (user_id === item.user_id) {
-          _unseen_detail.push({
-            user_id: item.user_id,
-            timestamp_from: 0,
-            timestamp_to: 0,
-            unread_count: 0
-          })
-        } else {
-          let timestamp_from = 0
-          let timestamp_to = 0
-          let unread_count = 0
-          if (item.unread_count === 0) {
-            timestamp_from = timestamp
-            timestamp_to = timestamp
-            unread_count = 1
-          } else {
-            timestamp_from = item.timestamp_from
-            timestamp_to = timestamp
-            unread_count = item.unread_count + 1
-          }
-          _unseen_detail.push({
-            user_id: item.user_id,
-            timestamp_from: timestamp_from,
-            timestamp_to: timestamp_to,
-            unread_count: unread_count
-          })
-        }
-      })
-
-      return _unseen_detail
-    }
-
-    if (action === "seen") {
-      const _unseen_detail = []
-      _.forEach(unseen_detail, (item) => {
-        if (user_id === item.user_id) {
-          _unseen_detail.push({
-            user_id: item.user_id,
-            timestamp_from: 0,
-            timestamp_to: 0,
-            unread_count: 0
-          })
-        } else {
-          _unseen_detail.push(item)
-        }
-      })
-
-      return _unseen_detail
-    }
-
-    if (action === "add_member") {
-      const _unseen_detail = [...unseen_detail]
-      _.forEach(member_add, (item) => {
-        _unseen_detail.push({
-          user_id: item,
-          timestamp_from: timestamp,
-          timestamp_to: timestamp,
-          unread_count: 1
-        })
-      })
-
-      return _unseen_detail
-    }
-
-    if (action === "delete_member") {
-      const _unseen_detail = unseen_detail.filter(
-        (item) => item.user_id !== member_remove
-      )
-
-      return _unseen_detail
-    }
-
-    return []
-  }
-
-  const handleSendNotification = (groupId, msg, dataGroups, receivers) => {
-    let notification_name = dataGroups.name
-    let icon = dataGroups.avatar
-      ? getPublicDownloadUrl(
-          `modules/chat/${groupId}/avatar/${dataGroups.avatar}`
-        )
-      : imageGroup
-    const link = `/chat/${groupId}`
-    const skipUrls = `/chat`
-    if (dataGroups.type === "employee") {
-      icon = userId * 1
-      notification_name = userFullName
-    }
-    let _msg = replaceHtmlMessage(msg)
-
-    if (dataGroups.type === "group") {
-      const fullNameSplit = userFullName.split(" ")
-      const fullNameSplitGroupMsg = fullNameSplit[fullNameSplit.length - 1]
-      const dot = msg.length > 25 ? "..." : ""
-      _msg = _msg.slice(0, 25) + dot
-      _msg = fullNameSplitGroupMsg + ": " + _msg
-    } else {
-      const dot = msg.length > 30 ? "..." : ""
-      _msg = _msg.slice(0, 30) + dot
-    }
-    socket.emit("chat_notification", {
-      receivers: receivers,
-      payload: {
-        title: notification_name,
-        body: _msg,
-        link: link,
-        icon: icon
-        //image: getPublicDownloadUrl("modules/chat/1_1658109624_avatar.webp")
-      },
-      data: {
-        skipUrls: skipUrls
-      }
-    })
-  }
-
-  const handleBreakType = async (groupId, timestamp, data) => {
-    const calculate_time_break = (timestamp, pre_timestamp, senderId) => {
-      let break_type = ""
-      const minute_diff = moment(timestamp).diff(
-        moment(pre_timestamp),
-        "minutes"
-      )
-
-      if (0 < minute_diff && minute_diff < 20) {
-        if (senderId === userId) {
-          break_type = "minute"
-        }
-      } else if (minute_diff >= 20) {
-        break_type = "line_time"
-      }
-
-      return break_type
-    }
-
-    let break_type = ""
-    // ** check forward
-    if (data.forward && !_.isEmpty(data.forward)) {
-      if (groupId === "") {
-        break_type = "line_time"
-      } else {
-        const q = query(
-          collection(db, `${firestoreDb}/chat_messages/${groupId}`),
-          orderBy("timestamp", "desc"),
-          limit(1)
-        )
-        await getDocs(q).then((res) => {
-          let dem = 0
-          res.forEach((docData) => {
-            dem++
-            if (dem === 1) {
-              const data_message = docData.data()
-              const pre_timestamp = data_message.timestamp
-              const senderId = data_message.sender_id
-              break_type = calculate_time_break(
-                timestamp,
-                pre_timestamp,
-                senderId
-              )
-            }
-          })
-          if (dem === 0) {
-            break_type = "line_time"
-          }
-        })
-      }
-    } else {
-      if (!_.isEmpty(chats)) {
-        const _chat = chats[chats.length - 1]
-        const pre_timestamp = _chat.time
-        const senderId = _chat.senderId
-        break_type = calculate_time_break(timestamp, pre_timestamp, senderId)
-      } else {
-        break_type = "line_time"
-      }
-    }
-
-    return break_type
-  }
-
-  const handleTimestampFile = (data) => {
-    let timestamp_link = 0
-    let timestamp_image = 0
-    let timestamp_file = 0
-    if (data.status && (data.status === "loading" || data.status === "error")) {
-      return { timestamp_link, timestamp_image, timestamp_file }
-    }
-
-    if (data.type === "link") {
-      timestamp_link = data.timestamp
-    }
-    if (data.type === "image" || data.type === "image_gif") {
-      timestamp_image = data.timestamp
-    }
-    if (
-      data.type === "file" ||
-      data.type === "video" ||
-      data.type === "audio"
-    ) {
-      timestamp_file = data.timestamp
-    }
-
-    return { timestamp_link, timestamp_image, timestamp_file }
-  }
-
   const sendMessage = async (groupId = "", msg, dataAddFile = {}) => {
-    setState({ checkAddMessage: true })
-    const timestamp = Date.now()
-    const file_count = handleCountFile(store.groups, groupId, dataAddFile.type)
-    const break_type = dataAddFile.break_type
-      ? dataAddFile.break_type
-      : await handleBreakType(groupId, timestamp, dataAddFile)
-
-    // ** encrypt
-    if (dataAddFile.message) {
-      delete dataAddFile.message
-    }
-    const msg_not_encrypt = msg
-    msg = AES.encrypt(msg, keyEncrypt).toString()
-    dataAddFile = { ...dataAddFile, encrypt: 1 }
-
-    if (!_.isEmpty(groupId)) {
-      delete dataAddFile.contact_id
-
-      const docGroups = doc(db, `${firestoreDb}/chat_groups/groups`, groupId)
-      getDoc(docGroups).then((res) => {
-        const dataGroups = res.data()
-        const unseen = dataGroups.user
-        const index = unseen.indexOf(userId)
-        if (index !== -1) {
-          unseen.splice(index, 1)
-        }
-        const unseen_detail = dataGroups.unseen_detail
-
-        const docData = {
-          message: msg,
-          sender_id: userId,
-          timestamp: timestamp,
-          type: "text",
-          break_type: break_type,
-          ...dataAddFile
-        }
-        if (docData.type === "text" || docData.type === "link") {
-          docData["_smeta"] = triGram(msg_not_encrypt)
-        }
-        setDoc(doc(collection(db, `${firestoreDb}/chat_messages/${groupId}`)), {
-          ...docData,
-          ...handleTimestampFile(docData)
-        })
-
-        // ** notification
-        const mute = dataGroups.mute.filter((item) => item !== userId)
-        const receivers = mute
-          .filter((x) => !unseen.includes(x))
-          .concat(unseen.filter((x) => !mute.includes(x)))
-        if (!_.isEmpty(dataAddFile.mention)) {
-          _.forEach(dataAddFile.mention, (val_mention) => {
-            if (!receivers.includes(val_mention) && val_mention !== userId) {
-              receivers.push(val_mention)
-            }
-          })
-        }
-        if (!_.isEmpty(receivers)) {
-          handleSendNotification(
-            groupId,
-            handleLastMessage(docData.type, msg_not_encrypt),
-            dataGroups,
-            receivers
-          )
-        }
-        let dataGroup = {
-          last_message: handleLastMessage(docData.type, msg_not_encrypt),
-          last_user: userId,
-          timestamp: timestamp,
-          new: 0,
-          unseen: unseen,
-          unseen_detail: setDataUnseenDetail(
-            "update",
-            userId,
-            timestamp,
-            unseen_detail,
-            []
-          )
-        }
-        if (!_.isEmpty(file_count)) {
-          dataGroup = { ...dataGroup, file_count: file_count }
-        }
-        handleUpdateGroup(groupId, dataGroup)
-      })
-    } else {
-      const contact = store.selectedUser.contact
-      let idEmployee = contact.idEmployee
-      let checkSetActive = true
-
-      if (dataAddFile.forward) {
-        if (
-          !_.isUndefined(dataAddFile.contact_id) &&
-          dataAddFile.contact_id !== ""
-        ) {
-          idEmployee = dataAddFile.contact_id
-          delete dataAddFile.contact_id
-          checkSetActive = false
-        } else {
-          return
-        }
-      }
-
-      setState({ loadingMessage: true })
-      const type = dataAddFile.type ? dataAddFile.type : "text"
-      let docData = {
-        last_message: handleLastMessage(type, msg_not_encrypt),
-        last_user: userId,
-        name: "",
-        timestamp: timestamp,
-        type: "employee",
-        user: [userId, idEmployee],
-        new: 1,
-        unseen: [idEmployee],
-        unseen_detail: setDataUnseenDetail(
-          "add",
-          userId,
-          timestamp,
-          [],
-          [userId, idEmployee]
-        )
-      }
-      if (!_.isEmpty(file_count)) {
-        docData = { ...docData, file_count: file_count }
-      }
-      handleAddNewGroup(docData).then((res) => {
-        const newGroupId = res.id
-        if (checkSetActive) {
-          setActive(newGroupId)
-        }
-        const docDataMessage = {
-          message: msg,
-          sender_id: userId,
-          timestamp: timestamp,
-          type: "text",
-          break_type: break_type,
-          ...dataAddFile
-        }
-        if (docDataMessage.type === "text" || docDataMessage.type === "link") {
-          docDataMessage["_smeta"] = triGram(msg_not_encrypt)
-        }
-
-        // ** notification
-        handleSendNotification(
-          newGroupId,
-          handleLastMessage(type, msg_not_encrypt),
-          docData,
-          idEmployee
-        )
-
-        setDoc(
-          doc(collection(db, `${firestoreDb}/chat_messages/${newGroupId}`)),
-          {
-            ...docDataMessage,
-            ...handleTimestampFile(docDataMessage)
-          }
-        )
-        setState({ loadingMessage: false })
-      })
-    }
+    const setCheckAddMessage = (value) => setState({ checkAddMessage: value })
+    const setLoadingMessage = (value) => setState({ loadingMessage: value })
+    await handleSendMessage(
+      groupId,
+      msg,
+      dataAddFile,
+      userId,
+      userFullName,
+      store.groups,
+      store.selectedUser?.contact?.idEmployee,
+      socket,
+      chats,
+      setCheckAddMessage,
+      setLoadingMessage,
+      setActive
+    )
 
     setEmptyHistory()
     setEmptyDataScrollBottom()
--- Frontend/src/layouts/separateSidebar/components/test/test1.js
+++ Frontend/src/layouts/separateSidebar/components/test/test1.js
@@ -1,10 +1,37 @@
-import React from "react"
-import { Card, CardBody } from "reactstrap"
+import React, { useContext } from "react"
+import { Button, Card, CardBody } from "reactstrap"
+import { handleSendMessage } from "@apps/modules/chat/common/firebaseCommon"
+import { useSelector } from "react-redux"
+import SocketContext from "utility/context/Socket"
 
 const test1 = () => {
+  const auth = useSelector((state) => state.auth)
+  const settingUser = auth.userData
+  const userId = settingUser.id
+  const userFullName = settingUser.full_name
+
+  const socket = useContext(SocketContext)
   return (
     <Card>
       <CardBody style={{ minHeight: "1500px" }}>test1</CardBody>
+
+      <Button
+        color="primary"
+        onClick={async () => {
+          return
+          await handleSendMessage(
+            "",
+            "vao ne",
+            {},
+            userId,
+            userFullName,
+            {},
+            "8",
+            socket
+          )
+        }}>
+        test chat
+      </Button>
     </Card>
   )
 }
