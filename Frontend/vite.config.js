6936d2b6499350a76883dad67d9980c6196376c3
Hải Long Trịnh <hailongtrinhlc@gmail.com>
2023-09-15 10:59:22 +0700

    save

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,14 +4,13 @@ import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
 import rollupNodePolyFill from "rollup-plugin-node-polyfills"
 import NodeGlobalsPolyfillPlugin from "@esbuild-plugins/node-globals-polyfill"
-import basicSsl from "@vitejs/plugin-basic-ssl"
 
 export default ({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
   const startPort = process.env.VITE_PORT ?? 3000
 
   const config = {
-    plugins: [react(), basicSsl()],
+    plugins: [react()],
     server: {
       https : true,
       port: startPort
