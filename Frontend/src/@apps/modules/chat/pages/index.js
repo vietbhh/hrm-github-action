dd8b78487ec966a3945f838f3dd6b292cfaf89b1
callmebaoxxxx <vuna@friday.vn>
2023-09-22 15:59:52 +0700

    done

--- Frontend/src/@apps/modules/chat/pages/index.js
+++ Frontend/src/@apps/modules/chat/pages/index.js
@@ -1211,6 +1211,12 @@ const AppChat = (props) => {
     }
   }, [])
 
+  useEffect(() => {
+    if (windowWidth < 767.98 && id === undefined) {
+      handleSidebar()
+    }
+  }, [id])
+
   return (
     <Fragment>
       <IdleTimerProvider timeout={1000 * 60} onAction={onAction}>
