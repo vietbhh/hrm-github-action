0f39ecec02b90d17e984ca6bfaa2f5ad8b214f76
Hải Long Trịnh <hailongtrinhlc@gmail.com>
2023-09-28 11:05:03 +0700

    Update init-app.sh

--- init-app.sh
+++ init-app.sh
@@ -8,17 +8,17 @@ if [[ -z $3 || -z $4 ]]; then
 printf "Initialing app by friday build server...\n"
 
 BUILD_SV=$2
-declare -A build=( [app_port]="3000" [app_url]="app.build.friday.vn" [php_port]="3001" [php_url]="be.build.friday.vn" [node_port]="3002" [node_url]="sv.build.friday.vn")
-declare -A build1=( [app_port]="3010" [app_url]="app1.build.friday.vn" [php_port]="3011" [php_url]="be1.build.friday.vn" [node_port]="3012" [node_url]="sv1.build.friday.vn")
-declare -A build2=( [app_port]="3020" [app_url]="app2.build.friday.vn" [php_port]="3021" [php_url]="be2.build.friday.vn" [node_port]="3022" [node_url]="sv2.build.friday.vn")
-declare -A build3=( [app_port]="3030" [app_url]="app3.build.friday.vn" [php_port]="3031" [php_url]="be3.build.friday.vn" [node_port]="3032" [node_url]="sv3.build.friday.vn")
-declare -A build4=( [app_port]="3040" [app_url]="app4.build.friday.vn" [php_port]="3041" [php_url]="be4.build.friday.vn" [node_port]="3042" [node_url]="sv4.build.friday.vn")
-declare -A build5=( [app_port]="3050" [app_url]="app5.build.friday.vn" [php_port]="3051" [php_url]="be5.build.friday.vn" [node_port]="3052" [node_url]="sv5.build.friday.vn")
-declare -A build6=( [app_port]="3060" [app_url]="app6.build.friday.vn" [php_port]="3061" [php_url]="be6.build.friday.vn" [node_port]="3062" [node_url]="sv6.build.friday.vn")
-declare -A build7=( [app_port]="3070" [app_url]="app7.build.friday.vn" [php_port]="3071" [php_url]="be7.build.friday.vn" [node_port]="3072" [node_url]="sv7.build.friday.vn")
-declare -A build8=( [app_port]="3080" [app_url]="app8.build.friday.vn" [php_port]="3081" [php_url]="be8.build.friday.vn" [node_port]="3082" [node_url]="sv8.build.friday.vn")
-declare -A build9=( [app_port]="3090" [app_url]="app9.build.friday.vn" [php_port]="3091" [php_url]="be9.build.friday.vn" [node_port]="3092" [node_url]="sv9.build.friday.vn")
-declare -A build10=( [app_port]="3110" [app_url]="app10.build.friday.vn" [php_port]="3111" [php_url]="be10.build.friday.vn" [node_port]="3112" [node_url]="sv10.build.friday.vn")
+declare -A build=( [app_port]="3000" [app_url]="app.build.friday.vn:86:86" [php_port]="3001" [php_url]="be.build.friday.vn:86" [node_port]="3002" [node_url]="sv.build.friday.vn:86")
+declare -A build1=( [app_port]="3010" [app_url]="app1.build.friday.vn:86" [php_port]="3011" [php_url]="be1.build.friday.vn:86" [node_port]="3012" [node_url]="sv1.build.friday.vn:86")
+declare -A build2=( [app_port]="3020" [app_url]="app2.build.friday.vn:86" [php_port]="3021" [php_url]="be2.build.friday.vn:86" [node_port]="3022" [node_url]="sv2.build.friday.vn:86")
+declare -A build3=( [app_port]="3030" [app_url]="app3.build.friday.vn:86" [php_port]="3031" [php_url]="be3.build.friday.vn:86" [node_port]="3032" [node_url]="sv3.build.friday.vn:86")
+declare -A build4=( [app_port]="3040" [app_url]="app4.build.friday.vn:86" [php_port]="3041" [php_url]="be4.build.friday.vn:86" [node_port]="3042" [node_url]="sv4.build.friday.vn:86")
+declare -A build5=( [app_port]="3050" [app_url]="app5.build.friday.vn:86" [php_port]="3051" [php_url]="be5.build.friday.vn:86" [node_port]="3052" [node_url]="sv5.build.friday.vn:86")
+declare -A build6=( [app_port]="3060" [app_url]="app6.build.friday.vn:86" [php_port]="3061" [php_url]="be6.build.friday.vn:86" [node_port]="3062" [node_url]="sv6.build.friday.vn:86")
+declare -A build7=( [app_port]="3070" [app_url]="app7.build.friday.vn:86" [php_port]="3071" [php_url]="be7.build.friday.vn:86" [node_port]="3072" [node_url]="sv7.build.friday.vn:86")
+declare -A build8=( [app_port]="3080" [app_url]="app8.build.friday.vn:86" [php_port]="3081" [php_url]="be8.build.friday.vn:86" [node_port]="3082" [node_url]="sv8.build.friday.vn:86")
+declare -A build9=( [app_port]="3090" [app_url]="app9.build.friday.vn:86" [php_port]="3091" [php_url]="be9.build.friday.vn:86" [node_port]="3092" [node_url]="sv9.build.friday.vn:86")
+declare -A build10=( [app_port]="3110" [app_url]="app10.build.friday.vn:86" [php_port]="3111" [php_url]="be10.build.friday.vn:86" [node_port]="3112" [node_url]="sv10.build.friday.vn:86")
 declare -a LISTS=(build build1 build2 build3 build4 build5 build6 build7 build8 build9 build10)
 
 APP_PORT="8000"
