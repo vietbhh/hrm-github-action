9220ee8de1638d062c40783430dec191056b9318
minhnv <minhnv@lifestud.io>
2023-11-16 13:42:08 +0700

    fix svg tag

--- Frontend/src/components/hrm/CreatePost/CreatePostDetails/TagYourColleagues.js
+++ Frontend/src/components/hrm/CreatePost/CreatePostDetails/TagYourColleagues.js
@@ -93,8 +93,8 @@ const TagYourColleagues = (props) => {
                     prepend={<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="vuesax/linear/search-normal">
                         <g id="search-normal">
-                        <path id="Vector" d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#696760" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path id="Vector_2" d="M22 22L20 20" stroke="#696760" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path id="Vector" d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z" stroke="#696760" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                        <path id="Vector_2" d="M22 22L20 20" stroke="#696760" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                         </g>
                         </g>
                         </svg>
@@ -199,8 +199,8 @@ const TagYourColleagues = (props) => {
                               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g id="vuesax/linear/close-circle">
                                   <g id="close-circle">
-                                    <path id="Vector" d="M6 18L18 6" stroke="#696760" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-                                    <path id="Vector_2" d="M18 18L6 6" stroke="#696760" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                    <path id="Vector" d="M6 18L18 6" stroke="#696760" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                                    <path id="Vector_2" d="M18 18L6 6" stroke="#696760" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                                   </g>
                                 </g>
                               </svg>
